932195f2fd2d484d061edc81c2bfb94b6accf98c
sudotx <64108848+sudotx@users.noreply.github.com>
2023-10-28 18:15:46 +0100

    chore:initial config

---
+++ src/MinimalPerp.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+contract MinimalPerpetual {
+    // data structures
+    // position
+
+
+    // functions
+    // increase/decrease postion, liquidate, deposit, withdraw, 
+}
